c57994afad992fa91493121c1c4ce669d864eea0
Matias Reyes <matias_reyes@icloud.com>
2021-04-07 13:38:28 -0400

    Created phonebook react app + added name input

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas',
+      id: 0
+    }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameInput = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={(addName)}>
+        <div>
+          name: <input value={newName} onChange={(handleNameInput)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul> 
+        {persons.map((person) => <li key={person}> {person.name} </li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
