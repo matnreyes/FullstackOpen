ded649539bcd496aebb4d6d2940df7f5d582fd42
Matias Reyes <matias_reyes@icloud.com>
2021-04-07 14:38:15 -0400

    Phone number logging added.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,15 +3,18 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas',
+      number: '(555)701-9832',
       id: 0
     }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
 
@@ -26,20 +29,21 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberInput  = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={(addName)}>
-        <div>
-          name: <input value={newName} onChange={(handleNameInput)}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={(handleNameInput)}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberInput}/></div>
+        <div> <button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul> 
-        {persons.map((person) => <li key={person.id}> {person.name} </li>)}
+        {persons.map((person) => <li key={person.id}> {person.name} {person.number}</li>)}
       </ul>
     </div>
   )
