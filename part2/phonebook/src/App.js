8552ebeecc467cab741ad27a795eb13e1e2a0615
Matias Reyes <matias_reyes@icloud.com>
2021-04-07 15:14:04 -0400

    Added search feature.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,8 +7,10 @@ const App = () => {
       id: 0
     }
   ]) 
+  const [ searchResult, setSearchResult ] = useState(persons)
+  const [ newSearch, setNewSearch ] = useState('')
   const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -17,7 +19,6 @@ const App = () => {
       number: newNumber,
       id: persons.length + 1
     }
-
     const match = persons.find((person) => person.name === nameObject.name)
     match ? window.alert(`${nameObject.name} is already in the phonebook`) : 
 
@@ -33,18 +34,27 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const searchNames = (event) => {
+    setNewSearch(event.target.value)
+    newSearch == '' ? 
+      setSearchResult(persons) :
+      setSearchResult(persons.filter((person) => person.name.toUpperCase() === event.target.value.toUpperCase()))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>Filter names: <input value={newSearch} onChange={searchNames}/></div>
+      </form> 
+      <h2>add a new</h2>
       <form onSubmit={(addName)}>
         <div>name: <input value={newName} onChange={(handleNameInput)}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberInput}/></div>
         <div> <button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      <ul> 
-        {persons.map((person) => <li key={person.id}> {person.name} {person.number}</li>)}
-      </ul>
+      <ul>{searchResult.map((person) => <li key={person.id}>{person.name} {person.number}</li>)}</ul>
     </div>
   )
 }
