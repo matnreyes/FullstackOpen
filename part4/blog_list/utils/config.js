fc4cb7342e54224618c9f278f5d22a856b60105e
Matias Reyes <matias_reyes@icloud.com>
2021-04-27 01:58:51 -0400

    separated middleware, config, and logger to own module

---
+++ part4/blog_list/utils/config.js
@@ -0,0 +1,11 @@
+const { builtinModules } = require('node:module')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = {
+    MONGODB_URI,
+    PORT
+}
