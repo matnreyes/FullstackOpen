c85ed332596fb4f250aafbf0852009af73c56944
Matias Reyes <matias_reyes@icloud.com>
2021-04-28 21:57:03 -0400

    Fixed formatting

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -1,11 +1,9 @@
-const { builtinModules } = require('node:module')
-
 require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
-    MONGODB_URI,
-    PORT
+  MONGODB_URI,
+  PORT
 }
