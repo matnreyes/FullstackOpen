6fdbb2c0d12d8043284e3e84fd72cd4b829c1053
Matias Reyes <matias_reyes@icloud.com>
2021-05-13 23:40:26 -0400

    Reformatted for linting

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -9,7 +9,13 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
   .then(() => {
     logger.info('connected to MongoDB')
   })
