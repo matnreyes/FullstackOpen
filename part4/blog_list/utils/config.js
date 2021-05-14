68653f255069757f90c602462e1c86acfa920f51
Matias Reyes <matias_reyes@icloud.com>
2021-05-14 12:13:22 -0400

    Properly connected backend to testing DB

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
-const MONGODB_URI = process.env === 'test'
-  ? process.env.TEST_MONGO_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
 module.exports = {
