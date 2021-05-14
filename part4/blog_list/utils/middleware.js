1e011b9bbc1d045da89567de0ed6181a66661e3d
Matias Reyes <matias_reyes@icloud.com>
2021-05-14 15:54:19 -0400

    update?

--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -18,14 +18,14 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
 }
 
 module.exports = {
-    requestLogger,
-    unknownEndpoint,
-    errorHandler
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
 }
