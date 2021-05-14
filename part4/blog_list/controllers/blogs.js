158c65d57d0dd5fb1a3949a80e0203838662edfa
Matias Reyes <matias_reyes@icloud.com>
2021-05-14 13:25:40 -0400

    Refactored get route to use async syntax

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,19 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 blogsRouter.get('/:id', (request, response, next) => {
   Blog.findById(request.params.id)
     .then(blog => {
       if (blog) {
-          response.json(blog)
+        response.json(blog)
       } else {
-          response.status(404).end()
+        response.status(404).end()
       }
     })
     .catch(error => next(error))
