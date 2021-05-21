a9e16b30607745e1f6c4b6d6049ef065626b38f3
Matias Reyes <matias_reyes@icloud.com>
2021-05-21 00:56:29 -0400

    Added check for password length and if user/pass are null

--- part4/blog_list/controllers/users.js
+++ part4/blog_list/controllers/users.js
@@ -10,6 +10,12 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (!body.username || !body.password) {
+    response.status(400).json({ error: 'field missing' })
+  } else if (body.password.length < 3) {
+    response.status(400).json({ error: 'password must be at least 3 characters' })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
