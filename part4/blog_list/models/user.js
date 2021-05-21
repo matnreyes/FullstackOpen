fe9f1c72871410e23045cf907dc37f91cbf03aee
Matias Reyes <matias_reyes@icloud.com>
2021-05-21 00:38:08 -0400

    new model for user info in db

---
+++ part4/blog_list/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  name: String,
+  passwordHash: String
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject._v
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
