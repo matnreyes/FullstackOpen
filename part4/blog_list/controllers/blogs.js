e50d8ea1280f12a27a3e45549f4a25c8130252a9
Matias Reyes <matias_reyes@icloud.com>
2021-05-26 23:58:26 -0400

    Blogs POST now adds user id and GET populates with user info

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,9 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.get('/:id', async (request, response) => {
@@ -18,15 +20,18 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const blog = new Blog({
+  const user = await User.find({})
+
+  const newBlog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user[0]._id
   })
 
-  const savedBlog = await blog.save()
-  response.json(savedBlog)
+  const savedBlog = await newBlog.save()
+  response.json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
