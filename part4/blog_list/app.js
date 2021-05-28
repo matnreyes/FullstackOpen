1acc61ea27f47dd15e84644d27f54f6055c0223b
Matias Reyes <matias_reyes@icloud.com>
2021-05-28 15:52:25 -0400

    app uses tokenExtractor middleware

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -27,6 +27,7 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
