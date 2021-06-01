0209425eb61f39f4a60124c9fa36e87a72d7db3e
Matias Reyes <matias_reyes@icloud.com>
2021-06-01 10:34:37 -0400

    Support for missing token

--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -5,6 +5,8 @@ const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     request.token = authorization.substring(7)
+  } else {
+    response.status(401).json({ error: 'no token provided' })
   }
   next()
 }
