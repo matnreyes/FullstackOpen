a133ca2f601905ec83243210bef5e91bdfd2180f
Matias Reyes <matias_reyes@icloud.com>
2021-06-06 23:19:48 -0400

    Sign in works and sends GET request for blogs

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,22 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const userInfo = await loginService.login({
+        username, password
+      })
+
+      blogService.setToken(userInfo.token)
+      window.localStorage.setItem(
+        'loggedBloglistUser', JSON.stringify(user)
+      )
+
+      setUsername('')
+      setPassword('')
+      setUser(user)
+
+      blogService.getAll().then(blogs =>
+        setBlogs(blogs))
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null
+      ? <Login username={username} password={password} setUsername={setUsername} setPassword={setPassword} submit={handleLogin}/>
+      : blogs.map(blog =>
+        <Blog key={blog.id} blog={blog}/>
+        )}
     </div>
   )
 }
