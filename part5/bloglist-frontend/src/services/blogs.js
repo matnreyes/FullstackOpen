34f1777926d156ab1b4f69cb567a60bff307d247
Matias Reyes <matias_reyes@icloud.com>
2021-06-06 23:19:13 -0400

    Blog token saved GET request pulls all blogs

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,19 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
+let token = null
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const setToken = newToken => {
+  token = `bearer ${newToken}`
 }
 
-export default { getAll }
+const getAll = async () => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const response = await axios.get(baseUrl, config)
+  return response.data
+}
+
+
+export default { getAll, setToken }
