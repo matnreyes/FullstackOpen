dc0a4f6a2f13e909b4ca24541b1a3071d689416e
Matias Reyes <matias_reyes@icloud.com>
2021-06-07 12:18:13 -0400

    Created component to submit new blog into system

---
+++ part5/bloglist-frontend/src/components/Submission.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Submission = ({title, setTitle, author, setAuthor, url, setUrl}) => (
+  <div>
+    <h2>Create new</h2>
+    <form>
+      <div>
+        title:
+        <input 
+        type="text"
+        value={title}
+        onChange={({target}) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+        <input
+        type="text"
+        value={author}
+        onChange={({target}) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+        <input 
+        type="text"
+        value={url}
+        onChange={({target}) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  </div>
+)
+
+export default Submission
