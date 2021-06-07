f9649ff16252f1b281fcc8cf77d8463376f0c73c
Matias Reyes <matias_reyes@icloud.com>
2021-06-07 00:42:23 -0400

    Users get logged in

--- part5/bloglist-frontend/src/components/Login.js
+++ part5/bloglist-frontend/src/components/Login.js
@@ -1,27 +1,27 @@
-import react from 'react'
+import React from 'react'
 
 const Login = ({ username, password, setUsername, setPassword, submit }) => (
-<div>
-  <form onSubmit={submit}>
-    <div>
-      username:
-      <input
-      type="text"
-      value={username}
-      onChange={({target}) => setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password:
-      <input
-      type="password"
-      value={password}
-      onChange={({target}) => setPassword(target.value)}
-      />
+  <div>
+    <form onSubmit={submit}>
+      <div>
+        username:
+        <input
+        type="text"
+        value={username}
+        onChange={({target}) => setUsername(target.value)}
+        />
       </div>
-      <button type="submit">Login</button>
-  </form>
-</div>
+      <div>
+        password:
+        <input
+        type="password"
+        value={password}
+        onChange={({target}) => setPassword(target.value)}
+        />
+        </div>
+        <button type="submit">Login</button>
+    </form>
+  </div>
 )
 
 export default Login
