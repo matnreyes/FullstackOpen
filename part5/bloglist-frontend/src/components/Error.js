02109bb439ebfc0a5299c9e890e8d06d1953be00
Matias Reyes <matias_reyes@icloud.com>
2021-06-07 16:29:35 -0400

    Error message component

---
+++ part5/bloglist-frontend/src/components/Error.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Error = error => {
+  const messageStyle = {
+    background: 'lightgrey',
+    fontSize: 20,
+    fontStyle: 'bold',
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    color: 'red'
+  }
+  return (
+    <div style={messageStyle}>
+      {error}
+    </div>
+  )
+}
+
+export default Error
