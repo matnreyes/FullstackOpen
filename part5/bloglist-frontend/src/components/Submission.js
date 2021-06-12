0b543216d009795295e5db2a05f9a933c20bcaae
Matias Reyes <matias_reyes@icloud.com>
2021-06-12 13:57:06 -0400

    Submission handler and state moved into Submission component

--- part5/bloglist-frontend/src/components/Submission.js
+++ part5/bloglist-frontend/src/components/Submission.js
@@ -1,36 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Submission = ({title, setTitle, author, setAuthor, url, setUrl, submit}) => (
-  <div>
-    <h2>Create new</h2>
-    <form onSubmit={submit}>
-      <div>
-        title:
-        <input 
-        type="text"
-        value={title}
-        onChange={({target}) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        author:
-        <input
-        type="text"
-        value={author}
-        onChange={({target}) => setAuthor(target.value)}
-        />
-      </div>
-      <div>
-        url:
-        <input 
-        type="text"
-        value={url}
-        onChange={({target}) => setUrl(target.value)}
-        />
-      </div>
-      <button type="submit">Create</button>
-    </form>
-  </div>
-)
+const Submission = ({addBlog}) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const createBlog = (event) => {
+    event.preventDefault()
+    addBlog({
+      title: title, 
+      author: author,
+      url: url
+    })
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  return (
+    <div>
+      <h2>Create new</h2>
+      <form onSubmit={createBlog}>
+        <div>
+          title:
+          <input 
+          type="text"
+          value={title}
+          onChange={({target}) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+          type="text"
+          value={author}
+          onChange={({target}) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input 
+          type="text"
+          value={url}
+          onChange={({target}) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+)}
 
 export default Submission
