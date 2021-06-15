6902d6ddef68acce3cf3c2bc49fbfbf12d08f39d
Matias Reyes <matias_reyes@icloud.com>
2021-06-15 00:02:49 -0400

    Added expanding component with CSS

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 const Blog = ({blog}) => {
-return (
-  <div>
-    {blog.title} {blog.author} <button onClick={}>Hide</button>
-  </div>  
+  const [expand, setExpand] = useState(false)
+
+  const toggleExpand = () => {
+    setExpand(!expand)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const expandedBlog = () => (
+    <div>
+      {blog.title} <button onClick={toggleExpand}>hide</button><br></br>
+      {blog.author}<br></br>
+      {blog.url}<br></br>
+      likes: {blog.likes} <button>like</button>
+    </div>
+  )
+
+  const smallBlog = () => (
+    <div>
+      {blog.title} <button onClick={toggleExpand}>view</button>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      { expand ? expandedBlog() : smallBlog() }
+    </div>  
 )}
 
 export default Blog
