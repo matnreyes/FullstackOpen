7323da9ea59fc9930ba660f9bcfe3f0b947ae32e
Matias Reyes <matias_reyes@icloud.com>
2021-06-30 19:16:54 -0400

    Tests for like button being clicked a limited number of times

--- part5/bloglist-frontend/src/tests/Blog.int.test.js
+++ part5/bloglist-frontend/src/tests/Blog.int.test.js
@@ -43,3 +43,34 @@ test('Blog displays all blog info when show button has been pressed', () => {
   expect(component.container).toHaveTextContent('200')
   expect(component.container).toHaveTextContent('blogurl.com')
 })
+
+test('Like button is clicked twice',  () => {
+  const blog = {
+    title: 'title of blog',
+    author: 'blog author',
+    url: 'blogurl.com',
+    likes: 200,
+    user: '123123'
+  }
+
+  const user = {
+    id: '123123'
+  }
+
+  const component = render(
+    <Blog blog={blog} user={user}/>
+  )
+
+  const mockHandler = jest.fn()
+
+  const expand = component.getByText('view')
+  fireEvent.click(expand)
+  const like = component.getByText('like')
+  like.onclick = mockHandler
+
+  for(let i = 0; i < 2; i++){
+    fireEvent.click(like)
+  }
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
