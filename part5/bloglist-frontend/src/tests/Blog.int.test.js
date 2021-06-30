32be65df095007874ceac268d13beaf96390069f
Matias Reyes <matias_reyes@icloud.com>
2021-06-30 17:39:24 -0400

    Test to see if button clicked expands the component

--- part5/bloglist-frontend/src/tests/Blog.int.test.js
+++ part5/bloglist-frontend/src/tests/Blog.int.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
 test('Blog renders title and author only', () => {
@@ -18,4 +18,28 @@ test('Blog renders title and author only', () => {
   expect(component.container).toHaveTextContent('This is the title of the blog')
   expect(component.container).toHaveTextContent('Blog author')
   expect(component.container).not.toHaveTextContent('blogurl.com')
+})
+
+test('Blog displays all blog info when show button has been pressed', () => {
+  const blog = {
+    title: 'This is the title of the blog',
+    author: 'Blog author',
+    url: 'blogurl.com',
+    likes: 200,
+    user: '12333'
+  }
+
+  const user = {
+    id: '3245234'
+  }
+
+  const component = render(
+    <Blog blog={blog} user={user}/>
+  )
+
+  const button = component.getByText('view')
+
+  fireEvent.click(button)
+  expect(component.container).toHaveTextContent('200')
+  expect(component.container).toHaveTextContent('blogurl.com')
 })
